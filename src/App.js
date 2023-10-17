7e3dbaa7fb3513e922368bed696ac1f81d694c9b
Jordan Threlfall <jordanjoannthrelfall@gmail.com>
2023-10-16 20:11:17 -0400

    Fixed app.js formatting and added nav bar to home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Home from './pages/home/Home';
+
 function App() {
   return (
     <BrowserRouter>
