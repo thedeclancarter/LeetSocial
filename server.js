d3f8b9a7d107faf464f5510f7666e6090b726d18
Declan Carter <declanpcarter@gmail.com>
2023-10-19 17:10:01 -0400

    Reorder server.js Routing

--- server.js
+++ server.js
@@ -3,89 +3,60 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-
-
 //MERN B
 const path = require('path');
-const PORT = process.env.PORT || 5102; 
+const PORT = process.env.PORT || 5102;
 
 //MERN A
-const app = express(); 
+const app = express();
 
 //MERN B
-app.set('port', (process.env.PORT || 5102)); 
-
+app.set('port', (process.env.PORT || 5102));
 
 //MERN B
-if (process.env.NODE_ENV === 'production')
-{
-// Set static folder
-app.use(express.static('frontend/build'));
-app.get('*', (req, res) =>
-{
-res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
 }
 
 
 //MERN A
 app.use(cors());
 app.use(bodyParser.json());
-app.use((req, res, next) =>
-{
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader(
-'Access-Control-Allow-Headers',
-'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-);
-res.setHeader(
-'Access-Control-Allow-Methods',
-'GET, POST, PATCH, DELETE, OPTIONS'
-);
-next();
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
 });
-
-
-//MERN B
-app.listen(PORT, () =>
-{
-console.log('Server listening on port ' + PORT);
-});
-
-//MERN B
-if (process.env.NODE_ENV === 'production')
-{
-// Set static folder
-app.use(express.static('frontend/build'));
-app.get('*', (req, res) =>
-{
-res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-});
-}
-
-
-
 
 //MERN A
-app.post('/api/logins', async (req, res, next) =>
-{
-// incoming: login, password
-// outgoing: id, firstName, lastName, error
-var error = '';
-const { login, password } = req.body;
-const db = client.db('Test');
-const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
-var id = -1;
-var fn = '';
-var ln = '';
-if( results.length > 0 )
-{
-id = results[0].id;
-fn = results[0].firstName;
-ln = results[0].lastName;
-}
-var ret = { id:id, firstName:fn, lastName:ln, error:''};
-res.status(200).json(ret);
+app.post('/api/login', async (req, res, next) => {
+    // incoming: login, password
+    // outgoing: id, firstName, lastName, error
+    var error = '';
+    const { login, password } = req.body;
+    const db = client.db('Test');
+    const results = await db.collection('Users').find({ Login: login, Password: password }).toArray();
+    var id = -1;
+    var fn = '';
+    var ln = '';
+    if (results.length > 0) {
+        id = results[0].id;
+        fn = results[0].firstName;
+        ln = results[0].lastName;
+    }
+    var ret = { id: id, firstName: fn, lastName: ln, error: '' };
+    res.status(200).json(ret);
 });
 
 
@@ -97,37 +68,50 @@ app.post('/api/signup', async (req, res) => {
     const db = client.db('Test');
     // Check if the username already exists in the database (assuming you want unique usernames).
     const existingUser = await db.collection('Users').findOne({ login });
-  
+
     if (existingUser) {
-      // If a user with the same username already exists, return an error.
-      return res.status(400).json({ error: 'Username already exists' });
+        // If a user with the same username already exists, return an error.
+        return res.status(400).json({ error: 'Username already exists' });
     }
-  
+
     // If the username is unique, create a new user in the database.
     const newUser = {
-      login,
-      password, // You should hash and salt the password for security.
-      firstName,
-      lastName
+        login,
+        password, // You should hash and salt the password for security.
+        firstName,
+        lastName
     };
-  
+
     // Insert the new user into the 'Users' collection.
     const result = await db.collection('Users').insertOne(newUser);
-  
+
     if (result.insertedCount === 1) {
-      // User registration was successful.
-      res.status(201).json({ message: 'User registered successfully' });
+        // User registration was successful.
+        res.status(201).json({ message: 'User registered successfully' });
     } else {
-      // If the insertion failed, return an error.
-      res.status(500).json({ error: 'User registration failed' });
+        // If the insertion failed, return an error.
+        res.status(500).json({ error: 'User registration failed' });
     }
-  });
-  
+});
+
 
 //MERN B
-
 require('dotenv').config();
 const url = process.env.MONGODB_URI;
-const MongoClient = require('mongodb').MongoClient; 
+const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(url);
 client.connect();
+
+//MERN B
+app.listen(PORT, () => {
+    console.log('Server listening on port ' + PORT);
+});
+
+//MERN B
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
