8b91967eac5e9a0cc38ec4359704c49a76585d8d
Ethan Releford <ethanrele@gmail.com>
2023-10-19 14:27:51 -0400

    login/signup apis complete

--- server.js
+++ server.js
@@ -6,14 +6,26 @@ const cors = require('cors');
 
 
 //MERN B
-const PORT = process.env.PORT || 5001; 
+const path = require('path');
+const PORT = process.env.PORT || 5102; 
 
 //MERN A
 const app = express(); 
 
 //MERN B
-app.set('port', (process.env.PORT || 5001)); 
+app.set('port', (process.env.PORT || 5102)); 
 
+
+//MERN B
+if (process.env.NODE_ENV === 'production')
+{
+// Set static folder
+app.use(express.static('frontend/build'));
+app.get('*', (req, res) =>
+{
+res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
+}
 
 
 //MERN A
@@ -55,7 +67,7 @@ res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
 
 
 //MERN A
-app.post('/api/login', async (req, res, next) =>
+app.post('/api/logins', async (req, res, next) =>
 {
 // incoming: login, password
 // outgoing: id, firstName, lastName, error
@@ -77,8 +89,45 @@ res.status(200).json(ret);
 });
 
 
-//MERN A
-const MongoClient = require('mongodb').MongoClient;
-const url ='';
+//ADDED SIGNUP API
+app.post('/api/signup', async (req, res) => {
+    // Incoming: username, password, firstName, lastName
+    const { login, password, firstName, lastName } = req.body;
+
+    const db = client.db('Test');
+    // Check if the username already exists in the database (assuming you want unique usernames).
+    const existingUser = await db.collection('Users').findOne({ login });
+  
+    if (existingUser) {
+      // If a user with the same username already exists, return an error.
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+  
+    // If the username is unique, create a new user in the database.
+    const newUser = {
+      login,
+      password, // You should hash and salt the password for security.
+      firstName,
+      lastName
+    };
+  
+    // Insert the new user into the 'Users' collection.
+    const result = await db.collection('Users').insertOne(newUser);
+  
+    if (result.insertedCount === 1) {
+      // User registration was successful.
+      res.status(201).json({ message: 'User registered successfully' });
+    } else {
+      // If the insertion failed, return an error.
+      res.status(500).json({ error: 'User registration failed' });
+    }
+  });
+  
+
+//MERN B
+
+require('dotenv').config();
+const url = process.env.MONGODB_URI;
+const MongoClient = require('mongodb').MongoClient; 
 const client = new MongoClient(url);
 client.connect();
