f39cceeff2b4dc9c7640154e800a71d9c065961e
Ethan Releford <ethanrele@gmail.com>
2023-10-19 10:45:36 -0400

    "currently on page 3/5 MERN B"

--- server.js
+++ server.js
@@ -1,47 +1,84 @@
+//MERN A
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const app = express();
-require('dotenv').config();
 
+
+
+//MERN B
+const PORT = process.env.PORT || 5001; 
+
+//MERN A
+const app = express(); 
+
+//MERN B
+app.set('port', (process.env.PORT || 5001)); 
+
+
+
+//MERN A
 app.use(cors());
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, DELETE, OPTIONS'
-    );
-    next();
+app.use((req, res, next) =>
+{
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader(
+'Access-Control-Allow-Headers',
+'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+);
+res.setHeader(
+'Access-Control-Allow-Methods',
+'GET, POST, PATCH, DELETE, OPTIONS'
+);
+next();
 });
 
-app.post('/api/login', async (req, res, next) => {
-    // incoming: login, password
-    // outgoing: id, firstName, lastName, error
-    var error = '';
-    const { login, password } = req.body;
-    const db = client.db('Test');
-    const results = await
-        db.collection('Users').find({ Login: login, Password: password }).toArray();
-    var id = -1;
-    var fn = '';
-    var ln = '';
-    if (results.length > 0) {
-        id = results[0].UserId;
-        fn = results[0].FirstName;
-        ln = results[0].LastName;
-    }
-    var ret = { id: id, firstName: fn, lastName: ln, error: '' };
-    res.status(200).json(ret);
+
+//MERN B
+app.listen(PORT, () =>
+{
+console.log('Server listening on port ' + PORT);
 });
 
+//MERN B
+if (process.env.NODE_ENV === 'production')
+{
+// Set static folder
+app.use(express.static('frontend/build'));
+app.get('*', (req, res) =>
+{
+res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
+}
+
+
+
+
+//MERN A
+app.post('/api/login', async (req, res, next) =>
+{
+// incoming: login, password
+// outgoing: id, firstName, lastName, error
+var error = '';
+const { login, password } = req.body;
+const db = client.db('Test');
+const results = await db.collection('Users').find({Login:login,Password:password}).toArray();
+var id = -1;
+var fn = '';
+var ln = '';
+if( results.length > 0 )
+{
+id = results[0].id;
+fn = results[0].firstName;
+ln = results[0].lastName;
+}
+var ret = { id:id, firstName:fn, lastName:ln, error:''};
+res.status(200).json(ret);
+});
+
+
+//MERN A
 const MongoClient = require('mongodb').MongoClient;
-const url = process.env.MONGODB_URI;
+const url ='';
 const client = new MongoClient(url);
 client.connect();
-
-app.listen(3000); // start Node + Express server on port 5000
