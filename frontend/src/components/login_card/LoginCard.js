b1b783dfb14b4f1c5d520fb5cf8e11d059015e2f
Jordan Threlfall <jordanjoannthrelfall@gmail.com>
2023-10-23 20:58:51 -0400

    Created login card to display login/register form

---
+++ frontend/src/components/login_card/LoginCard.js
@@ -0,0 +1,12 @@
+import './LoginCard.css';
+import Register from '../register/Register';
+
+export default function LoginCard() 
+{
+    
+    return(
+        <div>            
+            <Register />
+        </div>
+    );
+}
