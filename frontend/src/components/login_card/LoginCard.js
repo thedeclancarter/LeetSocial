37d0202ba08f7c2d3b68d24fec491fff66592113
Jordan Threlfall <jordanjoannthrelfall@gmail.com>
2023-10-24 23:24:58 -0400

    Implemented moving grid background and added logo to home page.

--- frontend/src/components/login_card/LoginCard.js
+++ frontend/src/components/login_card/LoginCard.js
@@ -1,12 +1,15 @@
+import React, { useEffect } from 'react';
 import './LoginCard.css';
 import Login from '../login/Login';
+import Logo from '../logo/Logo';
 
-export default function LoginCard() 
-{
-    
-    return(
-        <div>            
+export default function LoginCard() {
+
+    return (
+        <div className="grid-background">
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"></link>
             <Login />
+            <Logo />
         </div>
     );
 }
