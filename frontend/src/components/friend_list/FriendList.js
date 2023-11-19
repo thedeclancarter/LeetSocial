737416d54021ac45cec494394e6dd03e8fc6647d
Jordan Threlfall <jordanjoannthrelfall@gmail.com>
2023-11-18 22:15:57 -0500

    Implemented remove button for friends list and reformatted pages

--- frontend/src/components/friend_list/FriendList.js
+++ frontend/src/components/friend_list/FriendList.js
@@ -1,38 +1,28 @@
 import './FriendList.css';
+import Profile from '../../pages/profile/Profile';
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+var bp = require('../../path.js');
 
-// Temporary list while we wait for API
-const data = [
-    { name: "Anom" },
-    { name: "Megha" },
-    { name: "Subham" },
-    { name: "Anom" },
-    { name: "Megha" },
-    { name: "Subham" },
-    { name: "Anom" },
-    { name: "Megha" },
-    { name: "Subham" },
-    { name: "Anom" },
-    { name: "Megha" },
-    { name: "Subham" },
-]
+
 
 export default function FriendList() {
-    const [userData, setUserData] = useState({
-        firstName: '',
-        lastName: '',
-        leetCodeUsername: '',
-        userId: ''
-    });
+    const [searchTerm, setSearchTerm] = useState('');
+    const [showResults, setShowResults] = useState(false);
+    const [isMouseInside, setIsMouseInside] = useState(false);
+    const [searchData, setSearchData] = useState([]);
+
+    var _ud = sessionStorage.getItem('user_data');
+    var ud = JSON.parse(_ud);
+    var userId = ud.id;
+
+    const obj = { userId: userId };
+    const js = JSON.stringify(obj);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const _ud = sessionStorage.getItem('user_data');
-                const ud = JSON.parse(_ud);
-                const id = ud.id;
-                const obj = { userId: id };
-                const js = JSON.stringify(obj);
+
 
                 const response = await fetch('https://leet-social-2e5f98883d68.herokuapp.com/api/searchFriends', {
                     method: 'POST',
@@ -40,6 +30,7 @@ export default function FriendList() {
                     headers: { 'Content-Type': 'application/json' },
                 });
 
+                console.log(userId);
                 console.log(response);
 
                 if (!response.ok) {
@@ -49,42 +40,67 @@ export default function FriendList() {
                 const res = await response.json();
                 console.log(res);
 
-                const user = {
-                    firstName: res.firstName,
-                    lastName: res.lastName,
-                    username: res.leetCodeUsername,
-                    userId: res.userId,
-                };
+                setSearchData(res.map(user => ({
+                    ...user,
+                    name: `${user.firstName} ${user.lastName} (${user.leetCodeUsername})`
+                })));
             } catch (error) {
                 console.error('Error fetching data:', error);
                 alert(error.toString());
             }
         };
+        fetchData();
+    }, [searchTerm, userId]);
 
-        fetchData(); // Call the asynchronous function when the component mounts
-    }, []); // The empty dependency array ensures that this effect runs once after the initial render
-    // As soon as API is ready, add in the array of friends
+    const removeFriend = async (friendId) => {
+        try {
+            const apiPath = 'api/removeFriend';
+            await axios.post(bp.buildPath(apiPath), { userId, friendId });
 
-    
+            // Fetch the updated data after removal
+            const updatedResponse = await fetch('https://leet-social-2e5f98883d68.herokuapp.com/api/searchFriends', {
+                method: 'POST',
+                body: js,
+                headers: { 'Content-Type': 'application/json' },
+            });
+
+            if (!updatedResponse.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const updatedRes = await updatedResponse.json();
+            setSearchData(updatedRes.map(user => ({
+                ...user,
+                name: `${user.firstName} ${user.lastName} (${user.leetCodeUsername})`
+            })));
+        } catch (error) {
+            console.error('Error updating friend status', error);
+        }
+
+    };
+
+    console.log('searchData:', searchData);
+
     return (
         <div>
-
             <div className="friendList">
-                <table className="friendTable" >
-                    <tr className="titles">
-                        <th>Friend Name</th>
-                        <th>Remove</th>
-                    </tr>
-                    {data.map((val, key) => {
-                        return (
-                            <tr key={key}>
-                                <td>{val.name}</td>
-                                <td><button id="removeButton">Remove</button></td>
+                <table className="friendTable">
+                    <thead>
+                        <tr className="titles">
+                            <th>Friend Name</th>
+                            <th>Remove</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {searchData.map((item, index) => (
+                            <tr key={index} className="friend-list-row" >
+                                <td>{item.name}</td>
+                                <td><button id="removeButton" onClick={() => removeFriend(item.userId)}>Remove</button></td>
                             </tr>
-                        )
-                    })}
+                        ))}
+                    </tbody>
                 </table>
             </div>
         </div>
     );
-};
+}
