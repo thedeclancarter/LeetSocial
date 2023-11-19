ffd735badd51244eef1d6f7be7a040c1ba102fa6
Jordan Threlfall <jordanjoannthrelfall@gmail.com>
2023-11-18 19:58:42 -0500

    wired api to populate user info field

--- frontend/src/components/friend_list/FriendList.js
+++ frontend/src/components/friend_list/FriendList.js
@@ -1,5 +1,5 @@
 import './FriendList.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 // Temporary list while we wait for API
 const data = [
@@ -18,8 +18,54 @@ const data = [
 ]
 
 export default function FriendList() {
+    const [userData, setUserData] = useState({
+        firstName: '',
+        lastName: '',
+        leetCodeUsername: '',
+        userId: ''
+    });
 
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const _ud = sessionStorage.getItem('user_data');
+                const ud = JSON.parse(_ud);
+                const id = ud.id;
+                const obj = { userId: id };
+                const js = JSON.stringify(obj);
+
+                const response = await fetch('https://leet-social-2e5f98883d68.herokuapp.com/api/searchFriends', {
+                    method: 'POST',
+                    body: js,
+                    headers: { 'Content-Type': 'application/json' },
+                });
+
+                console.log(response);
+
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+
+                const res = await response.json();
+                console.log(res);
+
+                const user = {
+                    firstName: res.firstName,
+                    lastName: res.lastName,
+                    username: res.leetCodeUsername,
+                    userId: res.userId,
+                };
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                alert(error.toString());
+            }
+        };
+
+        fetchData(); // Call the asynchronous function when the component mounts
+    }, []); // The empty dependency array ensures that this effect runs once after the initial render
     // As soon as API is ready, add in the array of friends
+
+    
     return (
         <div>
 
